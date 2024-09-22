ea25af101a3e68ccb6675e8427649663eb48e91c
alibha04 <alibha.pra@gmail.com>
2024-09-23 02:27:53 +0530

    CRUD API

---
+++ routes/users.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+const router = express.Router();
+
+// Mock database
+/*const users = [
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@example.com',
+  },
+  {
+    first_name: 'Alice',
+    last_name: 'Smith',
+    email: 'alicesmith@example.com',
+  },
+];*/
+
+let users = [];
+
+
+//POST METHOD
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`${user.first_name} has been added to the Database`);
+})
+
+
+//DELETE USER
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    users = users.filter((user) => user.id !== id)
+  
+    res.send(`${id} deleted successfully from database`);
+  });
+
+//CREATE USER
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id)
+
+    res.send(foundUser)
+});
+
+//PATCH USER
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    const { first_name, last_name, email} = req.body;
+  
+    const user = users.find((user) => user.id === id)
+  
+    if(first_name) user.first_name = first_name;
+    if(last_name) user.last_name = last_name;
+    if(email) user.email = email;
+  
+    res.send(`User with the ${id} has been updated`)
+  
+  });
+
+  
+// Getting the list of users from the mock database
+router.get('/', (req, res) => {
+    res.send(users);
+})
+
+export default router
