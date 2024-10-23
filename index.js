a11aac821ac24f00349fc0dbf7171b941c3e4837
alibha04 <alibha.pra@gmail.com>
2024-10-23 04:12:43 +0000

    CRUD API

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import express from 'express';
-// import userRoutes from './routes/users.js'
+import userRoutes from './routes/users.js'
 import bodyParser from 'body-parser'
 const app = express();
 
